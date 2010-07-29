ea27e6bc028a7bc7c092ba5702b7aa883d97da63
optilude <optilude@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-07-29 15:49:54 +0000

    Initial checkin - ported from fourdigits.tinymcetemplates - installs a button, but no way to list the templates yet

---
+++ README.txt
@@ -0,0 +1,21 @@
+Introduction
+============
+
+``collective.tinymcetemplates`` is a Plone 3 add-on that provides a TinyMCE
+plugin that allows authors to insert *templates* and *snippets* into the page.
+
+Installation
+============
+
+Add ``collective.tinymcetemplates`` to your buildout in the ``eggs`` list, or
+as a dependency in the ``install_requires`` list in the ``setup.py`` file
+for a product that is already installed in your build. The package's
+configuration is loaded automatically by Plone.
+
+You must also install the product in Plone's Add-on control panel.
+
+Usage
+=====
+
+Content authors can use the new Template button to insert and edit templates.
+
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0b1'
+
+setup(name='collective.tinymcetemplates',
+      version=version,
+      description="TinyMCE Plugin for templates and snippets",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='TinyMCE Plone template snippet',
+      author='Rob Gietema',
+      author_email='rob@fourdigits.nl',
+      url='http://pypi.python.org/pypi/collective.tinymcetemplates',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.registry',
+          'Products.TinyMCE',
+      ],
+      extras_require={
+        'test': ['collective.testcaselayer', ],
+      },
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
